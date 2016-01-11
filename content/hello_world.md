c9a23b1ba61550b04d25e29f31f344c007f4f043
Patrick Mazulo <pmazulo@gmail.com>
2016-01-11 20:30:05 -0300

    Create pelican project

---
+++ content/hello_world.md
@@ -0,0 +1,11 @@
+title: Meu primeiro post
+date: 2016-1-11 07:33
+tags: news
+slug: hello_world
+author: Patrick Mazulo
+email: pmazuo@gmail.com
+summary: Meu primeiro post com Pelican
+
+# Hello World!
+
+Esse é meu primeiro post usando Pelican!
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Dunderlabs'
+SITENAME = '__labs__'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Fortaleza'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (
+    ('twitter', 'https://twitter.com/dunderlabs'),
+    ('github', 'https://github.com/dunderlabs'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+
+THEME = 'theme'
